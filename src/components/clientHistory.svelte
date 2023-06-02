863e2d05267c91ef31b4fd75c8ed1c6da00a05af
Dylan258 <dylan.courtney@stratusolve.com>
2023-06-02 10:43:21 +0200

    DevGym

---
+++ src/components/clientHistory.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Fa from "svelte-fa/src/fa.svelte";
+    import { faShareFromSquare } from "@fortawesome/free-solid-svg-icons";
+</script>
+
+<div class="card w-96 bg-base-100s">
+    <div class="card-body">
+        <h6>DateTime of interaction</h6>
+        <h2 class="card-title">Card title!</h2>
+        <p />
+        <div class="card-actions justify-end">
+            <button>
+                <Fa icon={faShareFromSquare} />
+            </button>
+        </div>
+    </div>
+</div>
--- src/routes/clientInteraction.svelte
+++ src/routes/clientInteraction.svelte
@@ -1,12 +1,21 @@
 <script>
     import MainFooter from "../lib/navigation/mainFooter.svelte";
     import DateTimeSnooze from "../components/dateTimeSnooze.svelte";
-
+    import ClientHistory from "../components/clientHistory.svelte";
 </script>
+
 <main class="artboard phone-6 p-5">
     <h1 class="text-purple-900 uppercase font-bold">Client Interaction</h1>
     <h3 class="text-base-content">Next client interaction</h3>
     <DateTimeSnooze />
+    <h1 class="text-purple-900 uppercase font-bold">COMMENTS/NOTES</h1>
+    <input
+        class="input w-full max-w-xs"
+        style="background-color: #f8f8f8;"
+        type="text"
+        placeholder="Make a new comment..." />
+    <h3 class="text-base-content">Client History</h3>
+    <ClientHistory />
 </main>
-    
+
 <MainFooter />
